d069157ae089fd7c41b01b901e9cca65a10f10a6
Troy SK <troy@troysk.com>
2016-05-10 20:56:32 +0530

    SlideShare oEmbed

--- api/resolver.js
+++ api/resolver.js
@@ -8,23 +8,18 @@ var _ = require('underscore');
 module.exports = function(req, res) {
   var url = req.query.url.trim();
 
-  // Giphy image urls are in the format:
-  // http://giphy.com/gifs/<seo-text>-<alphanumeric id>
-  var matches = url.match(/\-([a-zA-Z0-9]+)$/);
+  // SlideShare urls are in the format:
+  // http://slideshare.net/<username>/<presentation-slug>
+  var matches = url.match(/slideshare\.net\/([a-zA-Z0-9]+)\/([a-zA-Z0-9-]+)$/);
   if (!matches) {
     res.status(400).send('Invalid URL format');
     return;
   }
 
-  var id = matches[1];
-
   var response;
   try {
     response = sync.await(request({
-      url: 'http://api.giphy.com/v1/gifs/' + encodeURIComponent(id),
-      qs: {
-        api_key: key
-      },
+      url: 'https://www.slideshare.net/api/oembed/2?url=' + url + '&format=json',
       gzip: true,
       json: true,
       timeout: 15 * 1000
@@ -34,9 +29,15 @@ module.exports = function(req, res) {
     return;
   }
 
-  var image = response.body.data.images.original;
-  var width = image.width > 600 ? 600 : image.width;
-  var html = '<img style="max-width:100%;" src="' + image.url + '" width="' + width + '"/>';
+  // Extract iframe code
+  var html;
+  try {
+    html = response.body.html.match(/<iframe.*frame>/)[0];
+  } catch (e) {
+    res.status(500).send('Invalid response from SlideShare oEmbed');
+    return;
+  }
+
   res.json({
     body: html
     // Add raw:true if you're returning content that you want the user to be able to edit
